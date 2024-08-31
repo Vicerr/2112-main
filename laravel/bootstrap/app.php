c4dcf0ccf0c7564e831980a6a0fd5f4445029887
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2024-08-31 17:32:29 +0100

    feat: exclude product routes from middleware temporarily

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
-            'auth/*'
+            'auth/*',
+            'product/*'
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
