e19b1074681c09842128b539bc4a46a9e7ed8f5a
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2024-08-31 17:29:33 +0100

    fix: increase z-index in flash message and error

--- laravel/resources/views/components/error-message.blade.php
+++ laravel/resources/views/components/error-message.blade.php
@@ -1,5 +1,5 @@
 @if(session()->has('error'))
-<div id="flash-message" class="error--message hidden">
+<div id="flash-message" class="error--message">
   <span id="closebtn" class="closebtn">&times;</span>
     <b>
     {{session('error')}}
--- laravel/resources/views/components/flash-message.blade.php
+++ laravel/resources/views/components/flash-message.blade.php
@@ -1,5 +1,5 @@
 @if(session()->has('message'))
-<div id="flash-message" class="flash--message hidden">
+<div id="flash-message" class="flash--message">
   <span id="closebtn" class="closebtn">&times;</span>
     <b>
     {{session('message')}}
