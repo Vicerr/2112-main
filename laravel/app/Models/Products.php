3d970d4ec30c482639bca8d50e3726d896e48a73
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2024-09-07 07:47:22 +0100

    feat: add filter for product tags and specify local ket for one to many relationship

--- laravel/app/Models/Products.php
+++ laravel/app/Models/Products.php
@@ -12,6 +12,10 @@ class Products extends Model
     protected $primaryKey = 'product_id';
 
     public function scopeFilter($query, array $filters) {
+        if($filters['tag'] ?? false) {
+            $query->where('tag', 'like', '%' . request('tag') . '%');
+        }
+        
         if($filters['search'] ?? false) {
             $query->where('name', 'like', '%' . request('search') . '%')
                 ->orWhere('tag', 'like', '%' . request('search') . '%');
@@ -19,10 +23,10 @@ class Products extends Model
     }
 
     public function images() {
-        return $this->hasMany(Images::class, 'product_id');
+        return $this->hasMany(Images::class, 'product_id', 'id');
     }
     
     public function order_items() {
-        return $this->hasMany(Order_items::class, 'product_id');
+        return $this->hasMany(Order_items::class, 'product_id', 'id');
     }
 }
