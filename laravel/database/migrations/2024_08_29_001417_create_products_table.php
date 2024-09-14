b2c3c459a93f56a95fec3f98df44bd6f40ffa48f
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2024-09-14 14:19:38 +0100

    feat: add stock column to product migration

--- laravel/database/migrations/2024_08_29_001417_create_products_table.php
+++ laravel/database/migrations/2024_08_29_001417_create_products_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('desc');
             $table->string('color');
+            $table->string('stock');
             $table->unsignedBigInteger('price');
             $table->string('tag');
             $table->timestamps();
