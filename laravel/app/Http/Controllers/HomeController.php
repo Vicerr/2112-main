ce108139ec80595a27c07712ba4c75948f41f6c9
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2024-09-16 21:56:32 +0100

    style: correct typo in eloquent query

--- laravel/app/Http/Controllers/HomeController.php
+++ laravel/app/Http/Controllers/HomeController.php
@@ -10,13 +10,12 @@ class HomeController extends Controller
     public function index() {
         if (auth()->check()) {
             $user_id = auth()->user()->id;
-            $order = Orders::Where('user_id', $user_id)->first();
+            $order = Orders::where('user_id', $user_id)->first();
             $cart_count = count(json_decode($order->array_of_order_items));
             return view("index", ['cart' => $cart_count]);
         } else {
             $cart_count = '';
             return view("index", ['cart' => $cart_count]);
         }
-        
     }
 }
