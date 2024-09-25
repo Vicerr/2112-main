f86cb97ba021c49e2cbaa13d762950280370445d
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2024-09-25 11:58:08 +0100

    style: add favicon to login and register page

--- laravel/resources/views/users/login.blade.php
+++ laravel/resources/views/users/login.blade.php
@@ -5,7 +5,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>21/12 Sign-in</title>
+  <title>21/12 | Sign-in</title>
+  <link rel="icon" href="{{ asset('images/kaiadmin/favicon.ico') }}" type="image/x-icon" />
   <style>
     * {
       margin: 0;
--- laravel/resources/views/users/register.blade.php
+++ laravel/resources/views/users/register.blade.php
@@ -5,7 +5,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">\
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>21/12 Sign-up</title>
+  <title>21/12 | Sign-up</title>
+  <link rel="icon" href="{{ asset('images/kaiadmin/favicon.ico') }}" type="image/x-icon" />
   <style>
     * {
       margin: 0;
