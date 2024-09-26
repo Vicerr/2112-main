93c199aa66e6dbc7ca3fda666fa804101b66486b
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2024-09-26 14:50:46 +0100

    feat: create auth middleware

---
+++ laravel/app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!auth()->user()->hasRole('admin')) {
+            // Redirect or return a response if the user doesn't have the role
+            return redirect()->route('login'); // Replace with your desired redirect route
+        }
+        return $next($request);
+    }
+}
